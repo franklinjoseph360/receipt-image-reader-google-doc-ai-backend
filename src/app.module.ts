bf069c4a6bd291d9ac509fab0a03e110a9ab7b48
Franklin Joseph <franklinjoseph360@gmail.com>
2025-04-27 21:31:28 +0800

    feat: add nestjs/config and google/document-ai packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { ReceiptModule } from './receipt/receipt.module';
 
 @Module({
-  imports: [ReceiptModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ReceiptModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
