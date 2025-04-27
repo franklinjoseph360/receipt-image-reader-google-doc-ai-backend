b6a56e662d793fa754b42876bc56d1222bceb256
Franklin Joseph <franklinjoseph360@gmail.com>
2025-04-27 19:43:14 +0800

    feat: add receipt upload route, handle uploaded file

--- src/receipt/receipt.controller.ts
+++ src/receipt/receipt.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ReceiptService } from './receipt.service';
 
 @Controller('receipt')
-export class ReceiptController {}
+export class ReceiptController {
+  constructor(private readonly receiptService: ReceiptService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('receipt-file'))
+  async uploadReceipt(@UploadedFile() file: Express.Multer.File) {
+    console.log('receipt-file', file)
+    return file;
+  }
+}
